bdaac1d4932f0770b3180b5b5af57ef76afe1fda
NureAlam68 <nurealam151068@gmail.com>
2024-09-19 18:23:44 +0600

    added payooo home page

--- js/home.js
+++ js/home.js
@@ -12,4 +12,25 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
 
+    // step-3 verify pin number wrong way.. next a right ta sikbo
+    if(pinNumberInput === '1234') {
+        console.log('adding money to your account')
+
+        // step-4 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5 add moneyMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);  //string to number
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6 add money in dom or website
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add money ! Please try again.')
+    }
+
 });
