dcd24702e10a5020b5bd71a8ec7f7945299f58cf
NureAlam68 <nurealam151068@gmail.com>
2024-09-19 16:33:37 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -4,9 +4,21 @@
 document.getElementById('btn-login').addEventListener('click', function(event) {
         //step-2 stop loading browser
         event.preventDefault();
-        console.log('log in')
+        // console.log('log in')
 
-        //step-3 get the phone number
+        //step-3 get the phone number and pin
         const phoneNumber = document.getElementById('phone-num').value;
-        console.log(phoneNumber)
-})
+        const pinNumber = document.getElementById('pin-num').value;
+        // console.log(phoneNumber);
+        // console.log(pinNumber);
+
+        //step-4 number and pin match ..this is temporary. don,t like this
+        if(phoneNumber === '888000' && pinNumber === '1234') {
+            console.log('you are logged in');
+            //step-5 allow use this website
+            window.location.href = '/home.html'
+        } 
+        else {
+            alert('Wrong phone or pin!');
+        }
+});
